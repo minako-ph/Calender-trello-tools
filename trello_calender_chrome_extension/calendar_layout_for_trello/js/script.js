491404a12ec9cb3e5c666bf99bbca8a532061cc3
Minako Yamamoto <minako-ph@mf-minako-ph.local>
2023-01-15 00:55:07 +0900

    format

--- trello_calender_chrome_extension/calendar_layout_for_trello/js/script.js
+++ trello_calender_chrome_extension/calendar_layout_for_trello/js/script.js
@@ -1,9 +1,8 @@
 window.onload = function () {
-
     // 今日の日付
     let date = new Date();
-    let this_month = ("0"+(date.getMonth() + 1)).slice(-2);
-    let today = ("0"+date.getDate()).slice(-2);
+    let this_month = ("0" + (date.getMonth() + 1)).slice(-2);
+    let today = ("0" + date.getDate()).slice(-2);
 
     // ヘッダーの取得
     let header_of_header = document.getElementsByClassName('list-header-name');
